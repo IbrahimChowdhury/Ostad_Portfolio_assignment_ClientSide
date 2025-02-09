e2270e5d386a308a65a0596a815f1379e7a5aa2d
IbrahimChowdhury <ibrahimchowdhury1000@gmail.com>
2025-02-09 18:56:54 +0600

    wtf

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
 
 
 const inter = Inter({
@@ -23,7 +24,7 @@ export default function RootLayout({ children }) {
       >
         <Navbar/>
         {children}
-        <Footer />
+        <Footer/>
       </body>
     </html>
   );
